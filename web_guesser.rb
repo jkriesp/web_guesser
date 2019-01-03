8f9c5fafcb44d365d5daae42ae138f926cc8daa4
Jens Kristian Espevik <jenskristian@gmail.com>
2019-01-03 15:43:05 +0100

    don't show number unless correct

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,25 +6,28 @@ secret_number = rand(100)
 get '/' do
   guess = params['guess']
   guess_status = check_guess(guess, secret_number)
-  erb :index, :locals => {:number => secret_number, :guess_status => guess_status}
+  
   
 end
 
 def check_guess(guess, secret_number)
   if guess.to_i == secret_number
     guess_status = "You got it right!"
+    erb :index, :locals => {:number => secret_number, :guess_status => guess_status}
   elsif guess.to_i > secret_number
     if guess.to_i.between?(secret_number, secret_number + 5)
       guess_status = "Too high!"
     else
       guess_status = "Way too high!"
     end
+    erb :index, :locals => {:number => "X", :guess_status => guess_status}
   elsif guess.to_i < secret_number
     if guess.to_i.between?(secret_number - 5, secret_number)
       guess_status = "Too low"
     else
       guess_status = "Way too low!"
     end
+    erb :index, :locals => {:number => "X", :guess_status => guess_status}
   end
 end
 
