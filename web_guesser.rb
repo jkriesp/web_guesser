a4c56ea61df9132620d0953276114cf972cad145
Jens Kristian Espevik <jenskristian@gmail.com>
2019-01-03 09:46:15 +0100

    guess numbers via form input

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,6 +4,28 @@ require 'sinatra/reloader'
 secret_number = rand(100)
 
 get '/' do
-  erb :index, :locals => {:number => secret_number}
+  guess = params['guess']
+  guess_status = check_guess(guess, secret_number)
+  erb :index, :locals => {:number => secret_number, :guess_status => guess_status}
+  
 end
+
+def check_guess(guess, secret_number)
+  if guess.to_i == secret_number
+    guess_status = "You got it right!"
+  elsif guess.to_i > secret_number
+    if guess.to_i.between?(secret_number, secret_number + 5)
+      guess_status = "Too high!"
+    else
+      guess_status = "Way too high!"
+    end
+  elsif guess.to_i < secret_number
+    if guess.to_i.between?(secret_number - 5, secret_number)
+      guess_status = "Too low"
+    else
+      guess_status = "Way too low!"
+    end
+  end
+end
+
 
